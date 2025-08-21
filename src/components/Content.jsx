0bfd52e71be4b0ad95d9dfd6232886aca0ced400
치훈 <rlaclgns321@naver.com>
2025-08-21 14:34:54 +0900

    12일차_tailwindcss를 이용하여 OZ 페이지 디자인하기_ 완료

--- src/components/Content.jsx
+++ src/components/Content.jsx
@@ -1,10 +1,10 @@
 export default function Content({ content }) {
   return (
-    <div className="content-container">
-      <img src={content.img} alt={content.title} />
-      <span>모집중</span>
-      <div>{content.title}</div>
-      <p>{content.subtitle}</p>
+    <div className='flex flex-col items-start gap-[5px] content-container'>
+      <img src={content.img} alt={content.title} className='w-[300px] rounded-[10px] mb-[3px]' />
+      <span className='text-[12px] border-[1px] border-solid border-[#d7fa00] rounded-[3px] py-[4px] px-[5px] text-[#d7fa00]'>모집중</span>
+      <div className='text-[18px] font-[600] text-[#fff]'>{content.title}</div>
+      <p className='text-[12px] text-[#a0a0a0] '>{content.subtitle}</p>
     </div>
   );
 }
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,11 +1,11 @@
 export default function Header() {
   return (
-    <header className="header-container">
-      <h2>OZ코딩스쿨</h2>
-      <ul className="menu-container">
-        <li>로그인</li>
-        <li>회원가입</li>
-        <li>내클래스</li>
+    <header className='flex justify-between items-center bg-black py-[10px] px-[20px]'>
+      <h2 className='text-[#fff]'>OZ코딩스쿨</h2>
+      <ul className='flex justify-center items-center gap-[20px] menu-container'>
+        <li className='list-none font-normal text-[13px] text-[#fff]'>로그인</li>
+        <li className='list-none font-normal text-[13px] text-[#fff]'>회원가입</li>
+        <li className='list-none font-normal text-[13px] text-[#fff]'>내클래스</li>
       </ul>
     </header>
   );
